6cf440a2f49700470e8254a50922e49e04970842
David <daudeveloper@gmail.com>
2020-01-14 17:44:30 +0100

    Primer commit

---
+++ server/classes/usuarios.js
@@ -0,0 +1,35 @@
+class Usuarios {
+    constructor() {
+        this.personas = [];
+    }
+
+    agregarPersona(id, nombre, sala) {
+        let persona = { id, nombre, sala };
+
+        this.personas.push(persona);
+
+        return persona;
+    }
+
+    getPersona(id) {
+        return this.personas.filter(pers => pers.id === id)[0];
+    }
+
+    getPersonas() {
+        return this.personas;
+    }
+
+    getPersonasPorSala(sala) {
+        return this.personas.filter(pers => pers.sala === sala);
+    }
+
+    borrarPersona(id) {
+        let personaBorrada = this.getPersona(id);
+
+        this.personas = this.personas.filter(pers => pers.id != id);
+
+        return personaBorrada;
+    }
+}
+
+module.exports = { Usuarios };
